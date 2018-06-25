6fbd2cef74b700330eb8d5440a49a03e79da648a
Madhu Sudhan <madhu.subedi@monotype.com>
2018-06-25 17:23:28 +0545

    Changes on App.js by making variable to the text

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var welcome = "Welcome to the Road to learn React";
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{welcome}</h2>
       </div>
     );
   }
