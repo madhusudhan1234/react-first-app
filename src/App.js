4aeea53d812c068d3cb8ed52a5ab30ea66307122
Madhu Sudhan <madhu.subedi@monotype.com>
2018-07-10 19:11:12 +0545

    Composable Components

--- src/App.js
+++ src/App.js
@@ -53,35 +53,59 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
-          <input type="text" 
-            value={searchTerm}
-            onChange={this.onSearchChange} />
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item => {
-          const onHandleDismiss = () =>
-          this.onDismiss(item.objectID);
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={onHandleDismiss}
-                  tyoe="button"
-                >
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          );
-        })}
+        <Search 
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table 
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange, children } = this.props;
+    return (
+      <form>
+        {children} <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item => 
+          <div key={item.objectId}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button 
+                onClick={() => onDismiss(item.objectID)}
+                type="button">
+                Dismiss
+              </button>  
+            </span>
+          </div>
+        )}
+      </div>
+    )
   }
 }
 
