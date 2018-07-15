126547ebc2c792305b04c628f1aa562b58bd09c1
Madhu Sudhan <madhu.subedi@monotype.com>
2018-07-15 14:55:04 +0545

    Composoble components is completed - [x] Composoble components is completed

--- src/App.js
+++ src/App.js
@@ -67,64 +67,41 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children} <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    )
-  }
-}
+const Search = ({ value, onChange, children }) => 
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
-class Button extends Component {
-  render() {
-    const { 
-      onClick,
-      className = '',
-      children,
-    } = this.props;
+const Button = ({ onClick, className = '', children }) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >  
+    {children}
+  </button>
 
-    return (
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >  
-        {children}
-      </button>
-    );
-  }
-}
-
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => 
-          <div key={item.objectId}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>
-                Dismiss
-              </Button>  
-            </span>
-          </div>
-        )}
+const Table = ({ list, pattern, onDismiss }) =>
+  <div>
+    {list.filter(isSearched(pattern)).map(item => 
+      <div key={item.objectId}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectID)}>
+            Dismiss
+          </Button>  
+        </span>
       </div>
-    )
-  }
-}
+    )}
+  </div>
 
 export default App;
