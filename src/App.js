5276ca67f54fae8c9db72f962b563a7d1c16cc33
Madhu Sudhan <madhu.subedi@monotype.com>
2018-08-23 18:40:55 +0545

    Pagination is completed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
@@ -17,7 +20,6 @@ class App extends Component {
     this.state = {
       result: null,
       searchTerm: DEFAULT_QUERY,
-      searchTerm: '',
     };
 
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
@@ -34,11 +36,24 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+
+    const oldHits = page !== 0 
+      ? this.state.result.hits
+      : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+    
+    this.setState({ 
+      result: { hits: updatedHits, page }
+     });
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(e => e);
@@ -63,6 +78,7 @@ class App extends Component {
   
   render() {
     const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
 
     if (!result) { return null; }
 
@@ -83,6 +99,9 @@ class App extends Component {
               onDismiss={this.onDismiss}
             />
           }
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>  
         </div>
       </div>
     );
